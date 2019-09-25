92bfc96a96015df1bcf2bbbbb40c537340f8bde1
MAC DE MYRIAM <macdemyriam@MacBook-Air-de-MAC.local>
2019-09-25 19:49:13 +0200

    maquette sexy

--- src/Game/menu.cpp
+++ src/Game/menu.cpp
@@ -89,20 +89,38 @@ CS_Scene    *init_scene2(SDL_Renderer *render)
     scene->CS_setSceneColor(0xA9, 0xA9, 0xA9, 0);
     scene->CS_setTextColor(0xFF, 0xFF, 0xFF, 0xFF);
 
-    scene->CS_createButtonToScene(30, 12, 35, 10, 5, &Function1);
+    scene->CS_createButtonToScene(20, 6, 40, 7, 5, &Function1);
     scene->CS_writeTexte(-1, "UP = Z");
 
-    scene->CS_createButtonToScene(30, 12, 35, 27, 5, &Function1);
+    scene->CS_createButtonToScene(20, 6, 40, 15, 5, &Function1);
     scene->CS_writeTexte(-1, "DOWN = S");
 
-    scene->CS_createButtonToScene(30, 12, 35, 44, 5, &Function1);
+    scene->CS_createButtonToScene(20, 6, 40, 23, 5, &Function1);
     scene->CS_writeTexte(-1, "RIGHT = D");
 
-    scene->CS_createButtonToScene(30, 12, 35, 61, 5, &Function1);
+    scene->CS_createButtonToScene(20, 6, 40, 31, 5, &Function1);
     scene->CS_writeTexte(-1, "LEFT = Q");
     
-    scene->CS_createButtonToScene(50, 12, 25, 78, 5, &Function1);
+    scene->CS_createButtonToScene(30, 6, 35, 39, 5, &Function1);
     scene->CS_writeTexte(-1, "JUMP = SPACE");
+
+    scene->CS_createButtonToScene(30, 6, 35, 47, 5, &Function1);
+    scene->CS_writeTexte(-1, "DODGE = SPACE");
+    
+    scene->CS_createButtonToScene(20, 6, 40, 55, 5, &Function1);
+    scene->CS_writeTexte(-1, "ATT = M1");
+
+    scene->CS_createButtonToScene(20, 6, 40, 63, 5, &Function1);
+    scene->CS_writeTexte(-1, "ATT SP = M2");
+
+    scene->CS_createButtonToScene(20, 6, 40, 71, 5, &Function1);
+    scene->CS_writeTexte(-1, "USE = F");
+
+    scene->CS_createButtonToScene(20, 6, 40, 79, 5, &Function1);
+    scene->CS_writeTexte(-1, "SPELL 1 = A");
+
+    scene->CS_createButtonToScene(20, 6, 40, 86, 5, &Function1);
+    scene->CS_writeTexte(-1, "SPELL 2 = E");
 
     return (scene);
 }
