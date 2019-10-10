b271a90762879aac036479a3aa492b39e4cb3f83
Frédéric  Becerril <fbecerri@student.42.fr>
2019-10-10 18:41:47 +0200

    organize fill

--- src/Class/GameScene/AnimationsBank/bank.cpp
+++ src/Class/GameScene/AnimationsBank/bank.cpp
@@ -2,7 +2,12 @@
 
 void        CS_BankAnimation::loadNoMove(SDL_Renderer *render)
 {
-    noMove = loadAnimation("noMove", render, "resources/source/MCNoMoveL.png", "resources/source/MCNoMoveR.png", 10, 10, 45, 45, 1, 1, 1);
+    int h;
+    int y;
+    
+    h = 10 * gameSettings.resolution;
+    y = 50 - h / 2;
+    noMove = loadAnimation("noMove", render, "resources/source/MCNoMoveL.png", "resources/source/MCNoMoveR.png", 10, h, 45, y, 1, 1, 1);
 }
 
 void        CS_Character::loadBank(SDL_Renderer *render)
