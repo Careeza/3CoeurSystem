e1ddd9a083eba5c69e948fe5c2bb3fd442b6a4e3
Frédéric  Becerril <fbecerri@student.42.fr>
2019-11-19 00:51:25 +0100

    reorganization of files

--- src/Common/Class/EventControl/EventControl.cpp
+++ src/Common/Class/EventControl/EventControl.cpp
@@ -13,7 +13,7 @@ int    CS_KeyControl::loadEvenement()
     eventLoop = SDL_PollEvent(&event);
     type = event.type;
     return (eventLoop);
-}
+} // TODO
 
 /* int     CS_KeyControl::getMouseActions(int& x, int& y)
 {
