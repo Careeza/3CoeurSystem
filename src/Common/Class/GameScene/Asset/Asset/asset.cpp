3671522f6a6c59e8ebe6eeae66fe9e58f8ce8f6c
MAC DE MYRIAM <macdemyriam@MacBook-Air-de-MAC.local>
2019-11-29 11:24:13 +0100

    asset.cpp

---
+++ src/Common/Class/GameScene/Asset/Asset/asset.cpp
@@ -0,0 +1,49 @@
+# include "gameScene.h"
+
+CS_Asset::CS_Assets()
+{
+    size = new (SDL_Rect);
+}
+
+~CS_Assets()
+{
+    delete size;
+    delete texture;
+}
+
+void            CS_Asset::createAsset(SDL_Renderer *render, std::string source, float w, float h)
+{
+    SDL_Surface *surfaceAsset;
+
+    surfaceAsset = IMG_Load(source.c_str());
+    if (!surfaceAsset)
+    {
+        Tools->verbose(LEVEL1, "s", "error creating surface");
+        exit (0);
+    }
+    texture = SDL_CreateTextureFromSurface(render, surfaceAsset);
+	SDL_FreeSurface(surfaceAsset);
+
+    size->w = Tools->transformWidth(w);
+    size->h = Tools->transformHeight(h);
+}
+
+void            CS_Asset::addAsset(int zIndex)
+{
+
+}
+
+SDL_Texture     *CS_Asset::QueryTexture()
+{
+    return (texture);
+}
+
+SDL_Rect        *CS_Asset::QuerySize()
+{
+    return (size);
+}
+
+t_assetName     CS_Asset::QueryName()
+{
+    return (name);
+}
